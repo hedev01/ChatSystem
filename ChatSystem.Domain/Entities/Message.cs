5bff7033c8924233ca4d382acf56c9b8816f814b
Hedeve01 <nazari.fph@gmail.com>
2026-06-09 19:45:49 +0330

    Create Message Entity

---
+++ ChatSystem.Domain/Entities/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatSystem.Domain.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
+        public string Content { get; set; }
+        public DateTime SentAt { get; set; } = DateTime.UtcNow;
+    }
+}
