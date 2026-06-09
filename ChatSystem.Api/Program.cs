b6f78a2dcddae3fb0c0ca5d600eeb1693c321d5f
Hedeve01 <nazari.fph@gmail.com>
2026-06-09 19:46:20 +0330

    Configure EF Core

--- ChatSystem.Api/Program.cs
+++ ChatSystem.Api/Program.cs
@@ -1,3 +1,6 @@
+using ChatSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option
+        .UseSqlServer(
+            "Data Source=.;Initial catalog=Chat; Integrated Security=True;trustservercertificate=true;MultipleActiveResultSets=True;");
+});
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
