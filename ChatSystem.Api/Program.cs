fe1f1cc39c38d80989a72a4dab126cc3066409e5
Hedeve01 <nazari.fph@gmail.com>
2026-06-11 11:47:07 +0330

    Configure

--- ChatSystem.Api/Program.cs
+++ ChatSystem.Api/Program.cs
@@ -1,10 +1,15 @@
+using ChatSystem.Api.Hubs;
+using ChatSystem.Application.Interfaces;
+using ChatSystem.Application.UseCase;
+using ChatSystem.Domain.Interfaces;
 using ChatSystem.Infrastructure.Data;
+using ChatSystem.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSignalR();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -13,8 +18,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(option =>
 {
     option
         .UseSqlServer(
-            "Data Source=.;Initial catalog=Chat; Integrated Security=True;trustservercertificate=true;MultipleActiveResultSets=True;");
+            "Server=.;Database=Chat;User Id=UserForChatDB;Password=123456;TrustServerCertificate=True;");
 });
+
+builder.Services.AddScoped<IMessageRepository, MessageRepository>();
+builder.Services.AddScoped<IChatService, ChatService>();
 
 var app = builder.Build();
 
@@ -26,10 +34,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
