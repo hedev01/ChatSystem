20c6dad22042b95d1a957d67b3cc81d20095cd4b
Hedeve01 <nazari.fph@gmail.com>
2026-06-11 11:46:37 +0330

    Create Message Controller

---
+++ ChatSystem.Api/Controllers/MessageController.cs
@@ -0,0 +1,28 @@
+﻿using ChatSystem.Application.DTO;
+using ChatSystem.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly IChatService _chatService;
+
+        public MessageController(IChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage([FromBody] MessageDto dto)
+        {
+            await _chatService.SendMessage(dto);
+
+            return Ok();
+
+        }
+    }
+}
