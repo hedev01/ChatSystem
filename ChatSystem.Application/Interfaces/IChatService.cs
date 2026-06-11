b70c3f2c76e97efe44908c7c152822f90f891af4
Hedeve01 <nazari.fph@gmail.com>
2026-06-11 11:47:52 +0330

    Feat: Create Chat Service

---
+++ ChatSystem.Application/Interfaces/IChatService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatSystem.Application.DTO;
+
+namespace ChatSystem.Application.Interfaces
+{
+    public interface IChatService
+    {
+        Task SendMessage(MessageDto dto);
+    }
+}
---
+++ ChatSystem.Application/Service/ChatService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatSystem.Application.DTO;
+using ChatSystem.Application.Interfaces;
+using ChatSystem.Domain.Entities;
+using ChatSystem.Domain.Interfaces;
+
+namespace ChatSystem.Application.UseCase
+{
+    public class ChatService : IChatService
+    {
+        private readonly IMessageRepository _repo;
+        public ChatService(IMessageRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task SendMessage(MessageDto dto)
+        {
+            var message = new Message
+            {
+                SenderId = dto.SenderId,
+                ReceiverId = dto.ReceiverId,
+                Content = dto.Content
+            };
+
+            await _repo.AddAsync(message);
+        }
+    }
+}
