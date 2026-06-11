7a3d84bbeddb583cfe65522507ce82545ee09d6e
Hedeve01 <nazari.fph@gmail.com>
2026-06-11 11:47:30 +0330

    Feat: Create Message Dto

---
+++ ChatSystem.Application/DTO/MessageDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatSystem.Application.DTO
+{
+    public class MessageDto
+    {
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ ChatSystem.Application/DTO/SendMessageRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatSystem.Application.DTO
+{
+    public class SendMessageRequest
+    {
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
+        public string Content { get; set; }
+    }
+}
