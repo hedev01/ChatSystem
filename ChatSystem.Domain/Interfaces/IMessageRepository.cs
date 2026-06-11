5087d647a7d83c21ba88f305b666bb0e70569d12
Hedeve01 <nazari.fph@gmail.com>
2026-06-11 11:48:16 +0330

    Create Domain Interfaces

---
+++ ChatSystem.Domain/Interfaces/IMessageRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatSystem.Domain.Entities;
+
+namespace ChatSystem.Domain.Interfaces
+{
+    public interface IMessageRepository
+    {
+        Task AddAsync(Message message);
+        Task<List<Message>> GetConversationAsync(string user1, string user2);
+    }
+}
