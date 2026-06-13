cdd87f9d66800e3450ce10a465af113fe2df7d67
Hedeve01 <nazari.fph@gmail.com>
2026-06-13 19:55:48 +0330

    Clean DI

---
+++ ChatSystem.Api/DI/Dependencies.cs
@@ -0,0 +1,33 @@
+﻿using ChatSystem.Api.SignalR;
+using ChatSystem.Application;
+using ChatSystem.Application.Common.Security;
+using ChatSystem.Application.Features.Users.Register;
+using ChatSystem.Application.Interfaces;
+using ChatSystem.Application.UseCase;
+using ChatSystem.Domain.Interfaces;
+using ChatSystem.Infrastructure.Repositories;
+using ChatSystem.Infrastructure.Security;
+using ChatSystem.Infrastructure.Services;
+using FluentValidation;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.SignalR;
+using RegisterRequest = ChatSystem.Application.Features.Users.Register.RegisterRequest;
+
+namespace ChatSystem.Api.DI
+{
+    public class Dependencies
+    {
+        public static void Inject(IServiceCollection service)
+        {
+            service.AddSingleton<IUserIdProvider, CustomUserIdProvider>()
+                .AddScoped<IMessageRepository, MessageRepository>()
+                .AddScoped<IChatService, ChatService>()
+                .AddScoped<IJwtTokenService, JwtTokenService>()
+                .AddScoped<IUserRepository , UserRepository>()
+                .AddScoped<IPasswordHasher , PasswordHasher>()
+                .AddApplication()
+                .AddScoped<IValidator<RegisterRequest>, RegisterValidator>();
+
+        }
+    }
+}
