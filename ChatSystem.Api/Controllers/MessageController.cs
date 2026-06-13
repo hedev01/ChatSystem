5fe05a5a289de0f1f4a6ffe71c9b2c6ea0d999c1
Hedeve01 <nazari.fph@gmail.com>
2026-06-13 19:55:20 +0330

    Feat: Use IHubContext

--- ChatSystem.Api/Controllers/MessageController.cs
+++ ChatSystem.Api/Controllers/MessageController.cs
@@ -1,7 +1,9 @@
-﻿using ChatSystem.Application.DTO;
+﻿using ChatSystem.Api.Hubs;
+using ChatSystem.Application.DTO;
 using ChatSystem.Application.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 
 namespace ChatSystem.Api.Controllers
 {
@@ -11,7 +13,7 @@ namespace ChatSystem.Api.Controllers
     {
         private readonly IChatService _chatService;
 
-        public MessageController(IChatService chatService)
+        public MessageController(IChatService chatService , IHubContext<ChatHub> hubContext)
         {
             _chatService = chatService;
         }
