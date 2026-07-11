146b65d1ea0b625b8c7977da9b0e47b66b0ec99c
Hedeve01 <nazari.fph@gmail.com>
2026-07-11 12:35:48 +0330

    Feat: Create Publisher

---
+++ ChatSystem.Application/Interfaces/Messaging/IMessagePublisher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatSystem.Application.Events;
+
+namespace ChatSystem.Application.Interfaces.Messaging
+{
+    public interface IMessagePublisher
+    {
+        Task PublishAsync<T>(
+            T message,
+            CancellationToken cancellationToken = default)
+            where T : IntegrationEvent;
+    }
+}
---
+++ ChatSystem.Application/Interfaces/Messaging/IRabbitMqConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace ChatSystem.Application.Interfaces.Messaging
+{
+    public interface IRabbitMqConnection
+    {
+        IConnection Connection { get; }
+
+        IChannel Channel { get; }
+    }
+}
