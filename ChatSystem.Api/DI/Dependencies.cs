de5020b30712c6347274e9d86c05e5c95a9681c7
Hedeve01 <nazari.fph@gmail.com>
2026-07-11 12:32:35 +0330

    Feat: Add DI

--- ChatSystem.Api/DI/Dependencies.cs
+++ ChatSystem.Api/DI/Dependencies.cs
@@ -4,9 +4,12 @@ using ChatSystem.Application.Common.Security;
 using ChatSystem.Application.Features.Users.Login;
 using ChatSystem.Application.Features.Users.Register;
 using ChatSystem.Application.Interfaces;
+using ChatSystem.Application.Interfaces.Messaging;
 using ChatSystem.Application.Service;
 using ChatSystem.Application.UseCase;
 using ChatSystem.Domain.Interfaces;
+using ChatSystem.Infrastructure.Messaging;
+using ChatSystem.Infrastructure.Options;
 using ChatSystem.Infrastructure.Repositories;
 using ChatSystem.Infrastructure.Security;
 using ChatSystem.Infrastructure.Services;
@@ -20,19 +23,22 @@ namespace ChatSystem.Api.DI
 {
     public class Dependencies
     {
-        public static void Inject(IServiceCollection service)
+        public static void Inject(IServiceCollection service , IConfiguration configuration)
         {
+            service.Configure<RabbitMqOptions>(configuration.GetSection(RabbitMqOptions.SectionName));
             service.AddSingleton<IUserIdProvider, CustomUserIdProvider>()
                 .AddScoped<IMessageRepository, MessageRepository>()
                 .AddScoped<IChatService, ChatService>()
-                .AddScoped<IPresenceService , PresenceService>()
+                .AddScoped<IPresenceService, PresenceService>()
                 .AddScoped<IJwtTokenService, JwtTokenService>()
                 .AddScoped<IUserRepository, UserRepository>()
-                .AddSingleton<IUserConnectionRepository , InMemoryConnectionRepository>()
+                .AddSingleton<IUserConnectionRepository, InMemoryConnectionRepository>()
+                .AddSingleton<RabbitMqConnection>()
                 .AddScoped<IPasswordHasher, PasswordHasher>()
                 .AddApplication()
                 .AddScoped<IValidator<RegisterRequest>, RegisterValidator>()
-                .AddScoped<IValidator<LoginRequest>, LoginValidator>();
+                .AddScoped<IValidator<LoginRequest>, LoginValidator>()
+                .AddScoped<IMessagePublisher, RabbitMqPublisher>();
 
         }
     }
