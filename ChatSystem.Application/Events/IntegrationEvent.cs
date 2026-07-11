a92cba1cf053265e2152dcf2b48d67b02301b6a1
Hedeve01 <nazari.fph@gmail.com>
2026-07-11 12:34:52 +0330

    Create Event For RabbitMq

---
+++ ChatSystem.Application/Events/IntegrationEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatSystem.Application.Events
+{
+    public abstract record IntegrationEvent
+    {
+        public Guid EventId { get; init; } = Guid.NewGuid();
+
+        public DateTime OccurredOn { get; init; } = DateTime.UtcNow;
+    }
+}
---
+++ ChatSystem.Application/Events/UserRegisteredEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatSystem.Application.Events;
+
+namespace ChatSystem.Contracts.Events
+{
+    public sealed record UserRegisteredEvent(Guid userId, string fullName, string email) : IntegrationEvent;
+}
